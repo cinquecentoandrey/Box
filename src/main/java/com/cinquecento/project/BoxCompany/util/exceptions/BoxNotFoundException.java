de3f7859661775c3bada91a77b861178a1f38e03
cinquecentoandrey <chehonte16@gmail.com>
2022-08-20 12:15:22 +0300

    Necessary exceptions added

---
+++ src/main/java/com/cinquecento/project/BoxCompany/util/exceptions/BoxNotFoundException.java
@@ -0,0 +1,7 @@
+package com.cinquecento.project.BoxCompany.util.exceptions;
+
+public class BoxNotFoundException extends RuntimeException {
+    public BoxNotFoundException(String message) {
+        super(message);
+    }
+}
